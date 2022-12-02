c597df5c4c8ba3aa163ab9e69f46a0fa9c7292bb
Dat Pham <117200489+DatPham2010@users.noreply.github.com>
2022-12-02 15:43:30 +0700

    test eslint auto

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,6 @@
+import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
-
-import clsx from 'clsx'
 
 import styles from './Footer.module.scss'
 
--- components/nav/dropdown-item/index.tsx
+++ components/nav/dropdown-item/index.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
 import clsx from 'clsx'
+import Image from 'next/image'
 
 import styles from './DropdownItem.module.scss'
 
