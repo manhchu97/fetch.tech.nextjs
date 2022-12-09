d3cb506d985da4e3d3f3622de7f41d0e4e8342a6
Nguyen Duc Vuong <nguyenducvuonguet@gmail.com>
2022-12-09 15:52:50 +0700

    fix: add missing link href

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,5 +1,6 @@
 import { PORTAL_API } from '@config/global'
 import { API_LIST_PUBLIC_BLOG } from '@routes/api'
+import { PATH_CONFIG } from '@routes/paths'
 import BlogDetail from '@sections/blog/detail'
 import { IBlogItem, IDetailPostResponse, IListPostsResponse } from '@type/blog'
 import type { GetStaticPaths, InferGetStaticPropsType } from 'next'
@@ -59,7 +60,7 @@ const BlogDetailPage = ({
         title='Find the perfect fit with Fetch'
         subTitle='Find the perfect fit with Fetch'
         buttonText='Sign Up'
-        linkTo='/contact'
+        linkTo={PATH_CONFIG.contact}
       />
     </Page>
   )
