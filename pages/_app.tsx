7ef8cafdc0f6f5b351282bfb3d6452bbb20538d4
Hao Hoang-Duong <haoduong@fetch.tech>
2023-01-05 10:07:17 +0700

    feat: animation on scroll wrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 import Script from 'next/script'
 
 import { TrackingHeadScript } from '@phntms/next-gtm'
-import 'animate.css'
+import 'animate.css/animate.compat.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
