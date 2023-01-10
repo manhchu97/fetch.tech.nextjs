9f43f7c9113913142f1cacbef79347fd51fa2bd5
thucnguyen.fecth <thucnguyen@fetch.tech>
2023-01-10 11:18:03 +0700

    feat: add faq page

---
+++ pages/faq.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+import Page from '@/components/Page'
+
+const FAQPage: NextPage = () => {
+  return <Page title=''>FAQ Page</Page>
+}
+
+export default FAQPage
