fa7184bba3b62a6e6619c3c714199692ae5254ee
thucnguyen.fecth <thucnguyen@fetch.tech>
2023-01-31 11:32:06 +0700

    fix error null in job description

--- src/sections/job/detail/index.tsx
+++ src/sections/job/detail/index.tsx
@@ -95,9 +95,9 @@ const JobDetail = ({ fallback }: IJobDetailProps): React.ReactElement => {
 
   const jobDescription = useMemo(
     () =>
-      `${aboutFetch}${
-        client?.about || ''
-      }${responsibilities}${requirement}${niceToHave}${benefit}`,
+      `${aboutFetch}${client?.about || ''}${responsibilities || ''}${
+        requirement || ''
+      }${niceToHave || ''}${benefit || ''}`,
     [aboutFetch, client, responsibilities, requirement, niceToHave, benefit],
   )
 
