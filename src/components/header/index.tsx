ebd322c5f06bf04c1dc83ee1b842450e778faf33
thucnguyen.fecth <thucnguyen@fetch.tech>
2023-03-22 17:17:23 +0700

    feat: add gtta landing page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -49,7 +49,177 @@ const Header = ({ themeColor = '' }: IHeaderProps) => {
             isExpandContent && 'show-nav-container',
           )}
         >
-          <div className='nav-group'>
+          <div className='nav-menu-items'>
+            <div className='nav-group-items'>
+              <Link href={PATH_CONFIG.company}>
+                <a>
+                  <MenuItem title='For Companies' />
+                </a>
+              </Link>
+
+              <Link href={PATH_CONFIG.employees}>
+                <a>
+                  <MenuItem title='For Jobseekers' />
+                </a>
+              </Link>
+
+              <Link href={PATH_CONFIG.gtta}>
+                <a>
+                  <MenuItem title='For GTTA' />
+                </a>
+              </Link>
+            </div>
+
+            <div className='dropdown-container'>
+              <MenuItem
+                title='Services'
+                id='services'
+                hasIcon
+                dropdownMenu={
+                  <>
+                    <div className='dropdown-menu-content-container'>
+                      <Link href={PATH_CONFIG.services.view(1)}>
+                        <a>
+                          <DropdownItem
+                            title='Talent Acquisition'
+                            text='Procure talents from Vietnam'
+                            imageSrc='/images/nav/TalentAcquisition.png'
+                          />
+                        </a>
+                      </Link>
+
+                      <Link href={PATH_CONFIG.services.view(2)}>
+                        <a>
+                          <DropdownItem
+                            title='Services management'
+                            text='Employee directives and administration'
+                            imageSrc='/images/nav/ServiceManageMent.png'
+                          />
+                        </a>
+                      </Link>
+
+                      <Link href={PATH_CONFIG.services.view(3)}>
+                        <a>
+                          <DropdownItem
+                            title='Payroll and compliances'
+                            text='Streamlined contracting and payroll process'
+                            imageSrc='/images/nav/Payroll.png'
+                          />
+                        </a>
+                      </Link>
+
+                      <Link href={PATH_CONFIG.services.view(4)}>
+                        <a>
+                          <DropdownItem
+                            title='Full-suite project consultancy'
+                            text='Comprehensive projects management'
+                            imageSrc='/images/nav/FullSuite.png'
+                          />
+                        </a>
+                      </Link>
+                    </div>
+
+                    <div className='upperArrow'></div>
+                  </>
+                }
+              />
+            </div>
+
+            <div className='dropdown-container'>
+              <MenuItem
+                title='Resources'
+                id='resources'
+                hasIcon
+                dropdownMenu={
+                  <>
+                    <div className='dropdown-menu-content-container'>
+                      <Link href={PATH_CONFIG.resources.ourStory}>
+                        <a>
+                          <DropdownItem
+                            title='Our story'
+                            text='Learn more about us'
+                            imageSrc='/images/nav/OurStory.png'
+                          />
+                        </a>
+                      </Link>
+
+                      <Link href={PATH_CONFIG.resources.caseStudies.root}>
+                        <a>
+                          <DropdownItem
+                            title='Case Studies'
+                            text='Case studies'
+                            imageSrc='/images/nav/CaseStudies.png'
+                          />
+                        </a>
+                      </Link>
+
+                      <Link href={PATH_CONFIG.resources.calculator}>
+                        <a>
+                          <DropdownItem
+                            title='Vietnam Calculator'
+                            text='Assess estimated costs'
+                            imageSrc='/images/nav/Calculator.png'
+                          />
+                        </a>
+                      </Link>
+                    </div>
+
+                    <div className='upperArrow'></div>
+                  </>
+                }
+              />
+            </div>
+
+            <Link href={PATH_CONFIG.blog.root}>
+              <a>
+                <MenuItem title='Blogs' />
+              </a>
+            </Link>
+          </div>
+
+          <div className='nav-actions'>
+            <div className='header-contact-container'>
+              <Link href={PATH_CONFIG.contact}>
+                <a>
+                  <div role='button' className='header__contact-button'>
+                    <div className='contact-icon'>
+                      <Image
+                        src='/images/nav/contact.svg'
+                        alt='contact'
+                        layout='fill'
+                        objectFit='cover'
+                      />
+                    </div>
+
+                    <span className='header__contact-button__content'>
+                      Contact Us
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            </div>
+
+            <div className='whatsapp-container'>
+              <Link href='https://wa.me/6589334200'>
+                <a>
+                  <div role='button' className='whatsapp-button'>
+                    <div className='whatsapp-icon'>
+                      <Image
+                        src='/images/nav/whatsapp.svg'
+                        alt='whatsapp'
+                        layout='fill'
+                        objectFit='cover'
+                      />
+                    </div>
+
+                    <span className='whatsapp-button__content'>Whatsapp</span>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
+
+          {/* <div className='nav-group'>
             <div className='nav-group__first-container'>
               <Link href={PATH_CONFIG.company}>
                 <a>
@@ -60,6 +230,12 @@ const Header = ({ themeColor = '' }: IHeaderProps) => {
               <Link href={PATH_CONFIG.employees}>
                 <a>
                   <MenuItem title='For Jobseekers' />
+                </a>
+              </Link>
+
+              <Link href={PATH_CONFIG.employees}>
+                <a>
+                  <MenuItem title='For GTTA' />
                 </a>
               </Link>
             </div>
@@ -190,6 +366,18 @@ const Header = ({ themeColor = '' }: IHeaderProps) => {
               </a>
             </Link>
           </div>
+
+          <div className='header-contact-container'>
+            <Link href={PATH_CONFIG.contact}>
+              <a>
+                <div role='button' className='header__contact-button'>
+                  <span className='header__contact-button__content'>
+                    Whatsap
+                  </span>
+                </div>
+              </a>
+            </Link>
+          </div> */}
         </div>
 
         <div
