85723c02304e5299b8c60a0a0797439538fd9312
thucnguyen.fecth <thucnguyen@fetch.tech>
2023-03-27 14:40:39 +0700

    fix: change content when select any client service

--- src/sections/companies/how-it-work/FeatureCell.tsx
+++ src/sections/companies/how-it-work/FeatureCell.tsx
@@ -1,51 +1,47 @@
 import React from 'react'
 
 import Image from 'next/image'
-import Link from 'next/link'
 
 import clsx from 'clsx'
 
 import AnimatiopnOnScrollWrap from '@/components/AnimationOnScrollWrap'
 
 interface IFeatureCellProps {
-  feature: string
+  title: string
   icon: string
   color: string
   bgColor: string
-  url: string
+  onClick: () => void
 }
 
 const FeatureCell = ({
-  feature,
+  title,
   icon,
   color,
   bgColor,
-  url,
+  onClick,
 }: IFeatureCellProps): React.ReactElement => (
   <div className='col-6'>
-    <Link href={url}>
-      <a>
-        <AnimatiopnOnScrollWrap
-          render={(ref, animate) => (
-            <div
-              ref={ref}
-              className={clsx({
-                'feature-cell': true,
-                animate__animated: animate,
-                animate__fadeInUp: animate,
-              })}
-              style={{ color, backgroundColor: bgColor }}
-            >
-              <div className='icon-wrap'>
-                <Image src={icon} alt='check' width={36} height={36} />
-              </div>
+    <AnimatiopnOnScrollWrap
+      render={(ref, animate) => (
+        <div
+          ref={ref}
+          className={clsx({
+            'feature-cell': true,
+            animate__animated: animate,
+            animate__fadeInUp: animate,
+          })}
+          style={{ color, backgroundColor: bgColor }}
+          onClick={onClick}
+        >
+          <div className='icon-wrap'>
+            <Image src={icon} alt='check' width={36} height={36} />
+          </div>
 
-              <div className='h6-bold'>{feature}</div>
-            </div>
-          )}
-        />
-      </a>
-    </Link>
+          <div className='h6-bold'>{title}</div>
+        </div>
+      )}
+    />
   </div>
 )
 
