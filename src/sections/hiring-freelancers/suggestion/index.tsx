ab4484af53898da294a8e4769369bb2b652fb428
Nguyen Duc Vuong <nguyenducvuonguet@gmail.com>
2023-10-24 15:11:00 +0700

    fix: suggestion list

--- src/sections/hiring-freelancers/suggestion/index.tsx
+++ src/sections/hiring-freelancers/suggestion/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import clsx from 'clsx'
+import ldDebounce from 'lodash.debounce'
 
 import AnimatiopnOnScrollWrap from '@/components/AnimationOnScrollWrap'
+import ReactSlick from '@/components/ReactSlick'
 
 import styles from './Suggestion.module.scss'
 import SuggestionCard from './SuggestionCard'
@@ -193,6 +195,67 @@ type SuggestionProps = {
 const Suggestion = ({
   handleOpenClientInfoPopup,
 }: SuggestionProps): React.ReactElement => {
+  const [isDownLg, setIsDownLg] = useState(false)
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+
+  useEffect(() => {
+    let unmounted = false
+
+    const handleResize = ldDebounce(() => {
+      // https://stackoverflow.com/a/8876069
+      const width = Math.max(
+        document.documentElement.clientWidth,
+        window.innerWidth || 0,
+      )
+
+      if (unmounted) return
+
+      // md screen
+      setIsDownLg(width < 1200)
+    }, 100)
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      unmounted = true
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <div className={styles['suggestion-wrapper']}>
       <div className='suggestion-container'>
@@ -206,27 +269,51 @@ const Suggestion = ({
         </div>
 
         <div className='container suggestion-list'>
-          <div className='row g-4 justify-content-center'>
-            {LIST_CANDIDATES.map((item, index) => (
-              <AnimatiopnOnScrollWrap
-                key={index}
-                render={(ref, animate) => (
-                  <div
-                    ref={ref}
-                    className={clsx('col-sm-12 col-md-6 col-lg-4 col-xl-3', {
-                      animate__animated: true,
-                      animate__zoomIn: animate,
-                    })}
-                  >
-                    <SuggestionCard
-                      candidateInfo={item}
-                      handleOpenClientInfoPopup={handleOpenClientInfoPopup}
-                    />
-                  </div>
-                )}
-              />
-            ))}
-          </div>
+          {isDownLg ? (
+            <ReactSlick settings={settings}>
+              {LIST_CANDIDATES.map((item, index) => (
+                <AnimatiopnOnScrollWrap
+                  key={index}
+                  render={(ref, animate) => (
+                    <div
+                      ref={ref}
+                      className={clsx('h-100', {
+                        animate__animated: true,
+                        animate__zoomIn: animate,
+                      })}
+                    >
+                      <SuggestionCard
+                        candidateInfo={item}
+                        handleOpenClientInfoPopup={handleOpenClientInfoPopup}
+                      />
+                    </div>
+                  )}
+                />
+              ))}
+            </ReactSlick>
+          ) : (
+            <div className='row g-4 justify-content-center'>
+              {LIST_CANDIDATES.map((item, index) => (
+                <AnimatiopnOnScrollWrap
+                  key={index}
+                  render={(ref, animate) => (
+                    <div
+                      ref={ref}
+                      className={clsx('col-sm-12 col-md-6 col-lg-4 col-xl-3', {
+                        animate__animated: true,
+                        animate__zoomIn: animate,
+                      })}
+                    >
+                      <SuggestionCard
+                        candidateInfo={item}
+                        handleOpenClientInfoPopup={handleOpenClientInfoPopup}
+                      />
+                    </div>
+                  )}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
