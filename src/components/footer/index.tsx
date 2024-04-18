9c43adde5ccca9906f9fff74980c570a7547ddca
divmanh <divmanh@divmanhs-MacBook-Pro.local>
2024-04-18 15:57:36 +0700

    update whatapps

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -221,7 +221,7 @@ const Footer = () => {
             </a>
           </Link>
 
-          <Link href='tel:+6586069949' passHref>
+          <Link href='tel:+6589334200' passHref>
             <a className='img-social-container' rel='noreferrer'>
               <div className='img-social'>
                 <Image
